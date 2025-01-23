eaa08e167d0fc8dc08556f0a1b9e57cbe8839a58
Nodar Kobalia <nkobalia@borjomi.com>
2025-01-23 12:02:05 +0400

    upload APP

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, redirect, url_for, render_template
 from azure.storage.blob import BlobServiceClient, generate_blob_sas, BlobSasPermissions
 from azure.keyvault.secrets import SecretClient
-from azure.identity import ClientSecretCredential
 from azure.identity import DefaultAzureCredential
-from datetime import datetime, timedelta
+from azure.storage.blob import BlobServiceClient
+from datetime import datetime, timedelta, timezone
 from dotenv import load_dotenv
 import os
 
@@ -13,11 +13,11 @@ app = Flask(__name__)
 load_dotenv()
 
 # Configuration
-STORAGE_ACCOUNT_NAME = 'sharesafelystorage12'
-KEY_VAULT_NAME = 'nk-key-vuaalt'
-SECRET_NAME = 'blob-storage-account-key'
-CONTAINER_NAME = 'share'
-key_vault_url = 'https://nk-key-vuaalt.vault.azure.net/'
+STORAGE_ACCOUNT_NAME = os.getenv('STORAGE_ACCOUNT_NAME')
+KEY_VAULT_NAME = os.getenv('KEY_VAULT_NAME')
+SECRET_NAME = os.getenv('SECRET_NAME')
+CONTAINER_NAME = os.getenv('CONTAINER_NAME')
+key_vault_url = os.getenv('KEY_VAULT_URL')
 
 # Initialize the credential to use with Key Vault
 credential = DefaultAzureCredential()
@@ -30,16 +30,11 @@ secret = secret_client.get_secret(SECRET_NAME)
 
 ACCOUNT_KEY = secret.value
 
-# Construct the connection string for Azure Blob Storage
-connection_string = (
-    f"DefaultEndpointsProtocol=https;"
-    f"AccountName={STORAGE_ACCOUNT_NAME};"
-    f"AccountKey={ACCOUNT_KEY};"
-    f"EndpointSuffix=core.windows.net"
-)
-
 # Initialize BlobServiceClient with the connection string
-blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+blob_service_client = BlobServiceClient(
+    account_url='https://sharesafelystorage12.blob.core.windows.net/',
+    credential=credential
+)
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -54,9 +49,11 @@ def upload_file():
                 blob_name=file.filename,
                 account_key=ACCOUNT_KEY,  # Provide the account key here
                 permission=BlobSasPermissions(read=True),
-                expiry=datetime.utcnow() + timedelta(seconds=15)
+                starttime=datetime.now(timezone.utc),
+                expiry=datetime.now(timezone.utc) + timedelta(days=1)
             )
             sas_url = f"https://{STORAGE_ACCOUNT_NAME}.blob.core.windows.net/{CONTAINER_NAME}/{file.filename}?{sas_token}"
+            print(sas_token)
             return redirect(url_for('file_link', url=sas_url))
     return render_template('upload.html')
 
